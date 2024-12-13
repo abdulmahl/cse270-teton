91fcc8315e1c7a1a80cc012ac1a101faa4adb6e7
Abdul Mahl <mdu_abdul@icloud.com>
2024-12-13 12:34:50 +0200

    Added new smoke test suite export.

--- tests/test_smokeTestSuite.py
+++ tests/test_smokeTestSuite.py
@@ -26,8 +26,9 @@ class TestSmokeTestSuite():
     self.driver.find_element(By.ID, "username").click()
     self.driver.find_element(By.ID, "username").send_keys("Fred")
     self.driver.find_element(By.ID, "password").click()
-    assert self.driver.find_element(By.CSS_SELECTOR, ".errorMessage").text == "Invalid username and password."
     self.driver.find_element(By.ID, "password").send_keys("Qwerty123")
+    self.driver.find_element(By.CSS_SELECTOR, ".mysubmit:nth-child(4)").click()
+    WebDriverWait(self.driver, 30).until(expected_conditions.text_to_be_present_in_element((By.CSS_SELECTOR, ".errorMessage"), "Invalid username and password."))
   
   def test_directoryPage(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/directory.html")
